4b66eda5e01f05c11484de7bf607e6d98f3252f7
isotop786 <marufislam77@gmail.com>
2022-10-05 07:58:33 +0600

    Project init

---
+++ src/test/java/com/maruf/lra/ThymeleafLoginRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maruf.lra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafLoginRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
